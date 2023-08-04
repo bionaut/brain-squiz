1fa3d69cc328074bd141b7252dfc96e5c6e1032b
Jan Krajnak <janci.krajnak@gmail.com>
2023-08-04 17:52:50 +0200

    Frontend setup

---
+++ apps/game/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <div className={'flex flex-1 bg-red-100'}>
+      <h1>Game</h1>
+      <p>Welcome to game!</p>
+    </div>
+  )
+}
---
+++ apps/game/app/results/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div className={'flex flex-1 bg-red-100'}>
+      <h1>Results</h1>
+    </div>
+  )
+}
